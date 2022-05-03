832cf167a6d70b746d14b93196363901cfab0b23
Susanta <inbox.susanta@gmail.com>
2022-05-03 22:21:18 +0530

    View Basics & Passing Data

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, molestias?'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius amet saepe laborum veritatis dolorem ipsam maxime minima mollitia consequatur incidunt?'
+            ]
+        ]
+    ]);
 });
