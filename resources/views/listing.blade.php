a13fbffc660bac7f10c02e67b680993ed005cc87
Susanta <inbox.susanta@gmail.com>
2022-05-07 13:49:00 +0530

    Creating a Layout & Sections

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,4 @@
-    <h2>{{ $listing['title']}}</h2>
-    <p>{{ $listing['description']}}</p>
+@extends('layout') @section('content')
+<h2>{{ $listing["title"] }}</h2>
+<p>{{ $listing["description"] }}</p>
+@endsection
