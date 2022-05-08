402ade6bc12bf2a5f8e585bf81f9e7b8458e97de
Susanta <inbox.susanta@gmail.com>
2022-05-08 18:58:05 +0530

    Auth Links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,6 +29,16 @@
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            @auth
+            <li>
+                <span class="font-bold uppercase">
+                    Welcome {{ auth()->user()->name }}</span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i>
+                    Manage Listings</a>
+            </li>
+            @else
             <li>
                 <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
@@ -36,6 +46,7 @@
                 <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
+            @endauth
         </ul>
     </nav>
     <main>
