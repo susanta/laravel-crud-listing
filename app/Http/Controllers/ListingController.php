3b627aaa454c84bb9f14385a0ed7ba2c79c8a0b4
Susanta <inbox.susanta@gmail.com>
2022-05-08 12:16:06 +0530

    Flash Messages

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -44,7 +44,7 @@ class ListingController extends Controller
 
         Listing::create($formFields);
 
-        return redirect('/');
+        return redirect('/')->with('message', 'Listing created successfully!');
 
     }
 
